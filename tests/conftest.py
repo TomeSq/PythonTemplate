6c78457cb7d455020fb901d2e3b8ff3362a5f7fa
kenichi_shintome <kshintome@say-tech.co.jp>
2022-09-07 07:28:59 +0900

    ログ出力時に日時を出力するようにした。

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,28 @@
 import logging
 import logging.config
 
-# 設定ファイル(logging.conf)の読込
+LOGGING = {
+    "version": 1,
+    "formatters": {
+        "default": {"format": "%(asctime)s [%(levelname)s] %(message)s [%(module)s, %(lineno)d]"},
+        "uvicorn_format": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "format": "%(levelprefix)s %(asctime)s | %(message)s [%(module)s, %(lineno)d]",
+        },
+    },
+    "handlers": {
+        "default": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "stream": "ext://sys.stdout",
+            "formatter": "default",
+        }
+    },
+    "root": {"level": "DEBUG", "handlers": ["default"]},
+    "disable_existing_loggers": False,
+}
+
+logging.config.dictConfig(LOGGING)
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s [%(module)s, %(lineno)d]")
